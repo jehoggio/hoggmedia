20a7b32402657528bfd693384fb6b84ff2416d21
jeff <jeffhogg86@gmail.com>
2021-06-09 15:02:01 -0700

    center hero btn

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,14 +5,17 @@ const Hero = () => {
     <div className="max-w-7xl mt-12 mx-auto items-center lg:flex">
       <div className=" max-w-3xl pb-20 text-center">
         <div className="px-4">
-          <img src="images/hoggbanner.png" />
+          <div className="relative w-full">
+            <img src="images/hoggbanner.png" alt="banner" />
+          </div>
+
           <p className="mt-3 max-w-md mx-auto text-lg sm:text-xl md:mt-5 md:max-w-3xl">
             Coding, Tech and Video Production
           </p>
-          <div className="mt-8 sm:flex sm:justify-center lg:justify-start">
+          <div className="mt-8 sm:flex sm:justify-center">
             <div className="flex rounded-md shadow">
               <a
-                href="#"
+                href="/videos"
                 className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
               >
                 Videos
@@ -20,7 +23,7 @@ const Hero = () => {
             </div>
             <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
               <a
-                href="#"
+                href="/blog"
                 className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-600 md:py-4 md:text-lg md:px-10"
               >
                 Blog
@@ -30,7 +33,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="relative w-full lg:w-1/2">
-        <img src="images/hogglogo.png" alt="" />
+        <img src="images/hogglogo.png" alt="logo" />
       </div>
     </div>
   );
