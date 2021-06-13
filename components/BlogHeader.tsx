68ab6117d66847a64506dae3d02563882808c0d2
jeff <jeffhogg86@gmail.com>
2021-06-12 17:28:53 -0700

    cheange header

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -4,7 +4,7 @@ export default function BlogHeader({ title }) {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/blog">
-        <a className="hover:underline">Blog</a>
+        <a className="hover:underline">Hogg Media Blog</a>
       </Link>
     </h2>
   );
