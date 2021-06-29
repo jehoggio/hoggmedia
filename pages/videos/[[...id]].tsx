f313f160d91e46a8acd94c1d542f80d8d1e0986e
jeffhogg86 <jeffhogg86@gmail.com>
2021-06-29 14:39:08 -0700

    switch to getStaticProps

--- pages/videos/[[...id]].tsx
+++ pages/videos/[[...id]].tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import Head from 'next/head';
-import VideoList from '../../components/VideoList';
-import VideoPlayer from '../../components/VideoPlayer';
-import { YOUTUBE_ALL_VIDEOS_API } from '../../lib/videoConstants';
+import React from 'react'
+import Head from 'next/head'
+import VideoList from '../../components/VideoList'
+import VideoPlayer from '../../components/VideoPlayer'
+import { YOUTUBE_ALL_VIDEOS_API } from '../../lib/videoConstants'
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(
     `${YOUTUBE_ALL_VIDEOS_API}&key=${process.env.YOUTUBE_API_KEY}`
-  );
-  const data = await res.json();
+  )
+  const data = await res.json()
   if (!data) {
     return {
       notFound: true,
-    };
+    }
   }
   return {
     props: { data },
-  };
+  }
 }
 
 export default function Videos({ data }: any) {
@@ -24,12 +24,12 @@ export default function Videos({ data }: any) {
     <div>
       <Head>
         <title>Videos</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
         <VideoPlayer />
         <VideoList data={data} />
       </main>
     </div>
-  );
+  )
 }
