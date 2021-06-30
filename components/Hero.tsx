9329a133105444b224dd8e721e55e5cace45bf3c
jeffhogg86 <jeffhogg86@gmail.com>
2021-06-30 15:13:28 -0700

    update navbar, hero, add md files

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,35 +3,33 @@ import Link from 'next/link'
 
 const Hero = () => {
   return (
-    <div className='max-w-7xl mx-auto text-center shadow-2xl p-6 rounded-2xl'>
-      <div className='px-4'>
-        <div className='w-full bg-gray-900 p-2 rounded-lg'>
-          <Image
-            src='/images/hoggbanner2.png'
-            alt='banner'
-            width={1184}
-            height={155}
-          />
-        </div>
+    <div className='max-w-7xl bg-gray-900 mx-auto text-center shadow-2xl p-6 mt-20 rounded-2xl'>
+      <div className='w-full  p-2 rounded-lg'>
+        <Image
+          src='/images/hoggbanner2.png'
+          alt='banner'
+          width={1184}
+          height={155}
+        />
+      </div>
 
-        <p className='text-gray-300 mt-3 max-w-md mx-auto text-lg sm:text-xl md:mt-5 md:max-w-3xl'>
-          Web Development, Tech and Video Production
-        </p>
-        <div className='mt-8 sm:flex sm:justify-center sm:flex-shrink'>
-          <div className='mt-3 rounded-md shadow-lg  sm:mt-0 sm:ml-3'>
-            <Link href='/blog'>
-              <a className='bg-gray-900 text-gray-300 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-[#151B26] bg-white hover:bg-gray-300 md:py-4 md:text-lg md:px-10'>
-                Blog
-              </a>
-            </Link>
-          </div>
-          <div className='mt-3 rounded-md shadow-lg sm:mt-0 sm:ml-3'>
-            <Link href='/videos'>
-              <a className='bg-gray-900 text-gray-300 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-[#151B26] bg-white hover:bg-gray-300 md:py-4 md:text-lg md:px-10'>
-                Videos
-              </a>
-            </Link>
-          </div>
+      <p className='text-gray-300 mt-3 max-w-md mx-auto text-lg sm:text-xl md:mt-5 md:max-w-3xl'>
+        Web Development, Tech and Video Production
+      </p>
+      <div className='mt-8 sm:flex sm:justify-center sm:flex-shrink'>
+        <div className='mt-3 rounded-md shadow-lg  sm:mt-0 sm:ml-3'>
+          <Link href='/blog'>
+            <a className='text-gray-300 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md  bg-white hover:text-white hover:bg-gray-300 md:py-4 md:text-lg md:px-10'>
+              Blog
+            </a>
+          </Link>
+        </div>
+        <div className='mt-3 rounded-md shadow-lg sm:mt-0 sm:ml-3'>
+          <Link href='/videos'>
+            <a className='text-gray-300 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md  bg-white hover:text-white hover:bg-gray-300 md:py-4 md:text-lg md:px-10'>
+              Videos
+            </a>
+          </Link>
         </div>
       </div>
     </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
     <Disclosure as='nav' className='bg-gray-900'>
       {({ open }) => (
         <>
-          <div className='max-w-7xl mx-auto px-2 sm:px-6 '>
+          <div className='px-2 sm:px-6 '>
             <div className='relative flex items-center justify-between h-16'>
               <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
@@ -29,7 +29,7 @@ export default function Navbar() {
                 </Disclosure.Button>
               </div>
               <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
-                <div className='flex-shrink-0 flex items-center cursor-pointer'>
+                <div className='flex-shrink-0 flex items-center cursor-pointer '>
                   <Link href='/'>
                     <Image
                       src='/images/hogglogo.png'
@@ -54,7 +54,7 @@ export default function Navbar() {
                   </div>
                 </div>
               </div>
-              <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+              <div className='absolute inset-y-0 right-0 flex items-center mr-2 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
                 {/* Profile dropdown */}
                 <Menu as='div' className='ml-3 relative'>
                   {({ open }) => (
