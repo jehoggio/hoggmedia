b17aa6f3cc28ddb34342e871dd72a71a1ae0c9d4
jeffhogg86 <jeffhogg86@gmail.com>
2021-07-03 12:49:49 -0700

    adds categoryList

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,46 +1,41 @@
-import fs from 'fs'
-import path from 'path'
-import Link from 'next/link'
-import matter from 'gray-matter'
 import Layout from '@/components/Layout'
 import Post from '@/components/Post'
-import { sortByDate } from '@/utils/index'
+import CategoryList from '@/components/CategoryList'
+import { getPosts } from '@/lib/posts'
 
-export default function Blog({ posts }) {
+export default function Blog({ posts, categories }) {
   return (
     <Layout title='Blog'>
-      <h1 className='text-5xl border-b-4 p-5 font-bold mb-8 '>Blog</h1>
+      <div className='flex justify-between flex-col md:flex-row'>
+        <div className='md:w-3/4 md:mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold mb-8 '>Blog</h1>
 
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, i) => (
-          <Post key={i} post={post} />
-        ))}
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, i) => (
+              <Post key={i} post={post} />
+            ))}
+          </div>
+        </div>
+
+        <div className='invisible md:visible md:w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   )
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'))
+  const posts = getPosts()
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '')
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    )
-    const { data: frontmatter } = matter(markdownWithMeta)
-
-    return {
-      slug,
-      frontmatter,
-    }
-  })
+  // Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category)
+  const uniqueCategories = [...new Set(categories)]
 
   return {
     props: {
-      posts: posts.sort(sortByDate),
+      posts,
+      categories: uniqueCategories,
     },
   }
 }
