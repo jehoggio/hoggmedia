30a8046f8004c70d0cca9e55a5e5c821fc196b60
jeffhogg86 <jeffhogg86@gmail.com>
2021-07-17 20:23:39 -0700

    update default video

--- lib/videoConstants.ts
+++ lib/videoConstants.ts
@@ -1,7 +1,7 @@
-export const channelId = 'UCSTOp5FPjNXBmop3o47yZaQ';
-export const uploadsId = 'UUSTOp5FPjNXBmop3o47yZaQ';
-export const defaultVid = 'QRXOqXP-ZqE';
-export const playlistId = 'PLQnhPm2EJkhbo0nZHOblRzlQIzPhIMXvd';
-export const YOUTUBE_PLAYLISTS_API = `https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${channelId}`;
-export const YOUTUBE_PLAYLIST_ITEMS_API = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50`;
-export const YOUTUBE_ALL_VIDEOS_API = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${uploadsId}&part=snippet&maxResults=50`;
+export const channelId = 'UCSTOp5FPjNXBmop3o47yZaQ'
+export const uploadsId = 'UUSTOp5FPjNXBmop3o47yZaQ'
+export const defaultVid = 'yWuRDTg3VYo'
+export const playlistId = 'PLQnhPm2EJkhbo0nZHOblRzlQIzPhIMXvd'
+export const YOUTUBE_PLAYLISTS_API = `https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${channelId}`
+export const YOUTUBE_PLAYLIST_ITEMS_API = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50`
+export const YOUTUBE_ALL_VIDEOS_API = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${uploadsId}&part=snippet&maxResults=50`
