4473b552c209ec16b99b256e5d5254d3485d4385
jeffhogg86 <jeffhogg86@gmail.com>
2021-07-21 11:46:56 -0700

    add cloudinary

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from 'react'
 import Meta from './Meta'
-import Alert from './alert'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
@@ -8,14 +7,12 @@ type Props = {
   children?: ReactNode
   title?: string
   description?: string
-  preview?: boolean
 }
 
-const Layout = ({ preview, children, title, description }: Props) => (
+const Layout = ({ children, title, description }: Props) => (
   <div>
     <Meta title={title} description={description} />
     <Navbar />
-    <Alert preview={preview} />
     <main className='container mx-auto my-7'>{children}</main>
     <Footer />
   </div>
